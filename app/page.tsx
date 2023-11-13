b45ee5ca92c4d1de433be678f3781e7305ac5e97
mansi-padgelwar <mansi.padgelwar@airindia.com>
2023-11-13 16:37:48 +0530

    add urls

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    
-<h1>Hello world</h1>
-    
-
-   
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 4,
+        }}
+      >
+        <Link
+          href="https://demo-ssr-app-eta.vercel.app/en-us/halloween/app"
+          legacyBehavior
+        >
+          <a>1. Halloween Page</a>
+        </Link>
+        <Link
+          href="https://demo-ssr-app-eta.vercel.app/en-us/diwali/app"
+          legacyBehavior
+        >
+          <a>2. Diwali Page</a>
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
